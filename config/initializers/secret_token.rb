0cfb48b8bf5f773b817bc8ddd1f911b67ba6cebe
Beiowolf <robin_lin@beiowolf-mac.local>
2011-08-15 14:00:17 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elephant::Application.config.secret_token = '95dce2c5e2f034532a44eed0849e83fc59630a279c28cae3613f3e71525f6968c1be9ca1d9b69f5a7528acbc152e34c31506e47b826fb135acd267c2b7c8e1be'
